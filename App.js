a46c31ac2fb9ae60567a0ca97f742e80d6e98fd4
Rutvik <narigararutvik@gmail.com>
2023-03-06 10:39:05 +0530

    code update

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import AppNavigtor from './src/AppNavigtor';
-import Todo from './src/Sceens/Todo';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppNavigtor />
-      {/* <Todo/> */}
     </View>
   );
 }
